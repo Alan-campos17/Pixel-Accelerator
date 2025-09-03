6eb4156126743544b30e25fae2fb5c2cccc5c34b
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-09-03 15:06:54 -0300

    criando sprites

---
+++ src/js/characterSettings.js
@@ -0,0 +1,56 @@
+const settings = [
+  {
+    name: 'knight',
+    sprites: {
+      idle: {
+        imgSrc: './img/Idle/idle1.png',
+        framesMax: 11
+      },
+      run: {
+        imgSrc: './img/Run/run1.png',
+        framesMax: 6
+      },
+      walk: {
+        imgSrc: './img/Walk/walk1.png',
+        framesMax: 6
+      },
+      walk_attack: {
+        imgSrc: './img/Walk_Attack/walk_attack1.png',
+        framesMax: 6
+      },
+      jump: {
+        imgSrc: './img/Jump/jump1.png',
+        framesMax: 4
+      },
+      high_jump: {
+        imgSrc: './img/High_Jump/high_jump1.png',
+        framesMax: 6
+      },
+      climb: {
+        imgSrc: './img/Climb/climb1.png',
+        framesMax: 6
+      },
+      push: {
+        imgSrc: './img/Push/push1.png',
+        framesMax: 6
+      },
+      attack: {
+        imgSrc: './img/Attack/attack1.png',
+        framesMax: 6
+      },
+      run_attack: {
+        imgSrc: './img/Run_Attack/run_attack1.png',
+        framesMax: 6
+      },
+      takeHit: {
+        imgSrc: './img/Hurt/hurt1.png',
+        framesMax: 3
+      },
+      death: {
+        imgSrc: './img/Death/death1.png',
+        framesMax: 6
+      }
+    },
+    perfil: './img/knight.png'
+  }
+];
